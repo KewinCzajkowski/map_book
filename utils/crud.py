48c64d3ab2b48db6e7ba09461598cfa77edc0790
KewinCzajkowski <166690486+KewinCzajkowski@users.noreply.github.com>
2024-04-18 10:49:14 +0200

    wip: add a function to create user

--- utils/crud.py
+++ utils/crud.py
@@ -2,3 +2,9 @@ def show_users(user_list: list[dict]) -> None:
     for user in user_list:
         print(f"Twój znajomy {user['name']} opublikował: {user['posts']} postów")
 
+def add_new_user(users: list)-> None:
+    new_name: str = input("Enter your name: ")
+    new_surname: str = input("Enter your surname: ")
+    new_posts: int = int(input("Enter your posts: "))
+    new_user: dict = {"name": new_name, "surname": new_surname, "posts": new_posts}
+    users.append(new_user)
